8119e3463d4a39f61b77c34f1a6e8beea3dd8509
Cobchise <cobchise@pm.me>
2025-11-23 19:08:56 -0800

    Added footer icons for twitch/youtube. Added lucide-svelte and simple icons for svelte (for brands)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import Logo from '$lib/assets/cob.png';
-    import Twitch from '$lib/Twitch.svelte'
+    import TwitchClips from '$lib/TwitchClips.svelte'
+    import { SiTwitch, SiYoutube } from '@icons-pack/svelte-simple-icons'
 </script>
 
 <!-- NavBar Start -->
@@ -57,7 +58,7 @@
 
 <!-- Clips Start -->
 
-<Twitch />
+<TwitchClips />
 
 <!-- Clips End -->
 
@@ -68,19 +69,12 @@
 		<p>Copyright © {new Date().getFullYear()} - All right reserved</p>
 	</aside>
 	<nav class="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
-		<a>
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				width="24"
-				height="24"
-				viewBox="0 0 24 24"
-				class="fill-current"
-			>
-				<path
-					d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"
-				></path>
-			</svg>
+		<a href="https://www.twitch.tv/cobchise" target="_blank">
+            <SiTwitch />
 		</a>
+        <a href="https://www.youtube.com/@cobchise7464" target="_blank">
+            <SiYoutube />
+        </a>
 	</nav>
 </footer>
 
